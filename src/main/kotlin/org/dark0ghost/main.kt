7439f41b726d117d39d3d2aa61860ed45442953a
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-07-05 23:08:54 +0300

    fix style

--- src/main/kotlin/org/dark0ghost/main.kt
+++ src/main/kotlin/org/dark0ghost/main.kt
@@ -8,11 +8,11 @@ import java.net.InetAddress
 import java.net.InetSocketAddress
 
 private val host: String = InetAddress.getLocalHost()?.toString()?: "127.0.0.1"
-private const val port: Int = 600
+private const val PORT: Int = 600
 
 suspend fun main() {
     val selectorManager = ActorSelectorManager(Dispatchers.IO)
-    val address = InetSocketAddress(host,port)
+    val address = InetSocketAddress(host,PORT)
     val apiKtor = KtorApi.Builder().setSelector(selectorManager).setAddress(address).build()
     val eye: Eye = Eye.Builder().setApi(apiKtor).build()
 }
