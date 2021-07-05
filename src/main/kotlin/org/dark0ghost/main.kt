7977d864ab78819cc12176d0f926a400dbc78756
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-07-05 23:03:29 +0300

    fix style

--- src/main/kotlin/org/dark0ghost/main.kt
+++ src/main/kotlin/org/dark0ghost/main.kt
@@ -2,10 +2,13 @@ package org.dark0ghost
 
 import io.ktor.network.selector.ActorSelectorManager
 import kotlinx.coroutines.Dispatchers
+import org.dark0ghost.eye.Eye
+import org.dark0ghost.ktorApi.KtorApi
 import java.net.InetSocketAddress
 
 suspend fun main() {
     val selectorManager = ActorSelectorManager(Dispatchers.IO)
     val address = InetSocketAddress("127.0.0.1",6000)
-    //val apiKtor = KtorApi.Builder().setSelector(selectorManager).setAddress(address).build()
+    val apiKtor = KtorApi.Builder().setSelector(selectorManager).setAddress(address).build()
+    val eye: Eye = Eye.Builder().setApi(apiKtor).build()
 }
