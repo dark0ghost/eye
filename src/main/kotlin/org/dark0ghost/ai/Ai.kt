8942aa61f4c7de071e18df124a1b8e63a20b32e9
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-07-23 23:32:43 +0300

    add converter

--- src/main/kotlin/org/dark0ghost/ai/Ai.kt
+++ src/main/kotlin/org/dark0ghost/ai/Ai.kt
@@ -1,10 +1,13 @@
 package org.dark0ghost.ai
 
+import org.bytedeco.javacv.OpenCVFrameConverter
 import org.bytedeco.opencv.global.opencv_imgcodecs.imread
 import org.bytedeco.opencv.opencv_core.Mat
 
 
 class Ai {
+    private val converter: OpenCVFrameConverter.ToMat =  OpenCVFrameConverter.ToMat();
+
     private fun loadImage(fileName: String): Mat = imread(fileName)
 
 }
