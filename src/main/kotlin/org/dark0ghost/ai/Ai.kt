74ef4b99fa617941564c7e9dca6468527fdcf120
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-07-23 23:24:28 +0300

    fix

--- src/main/kotlin/org/dark0ghost/ai/Ai.kt
+++ src/main/kotlin/org/dark0ghost/ai/Ai.kt
@@ -1,11 +1,10 @@
 package org.dark0ghost.ai
 
 import org.bytedeco.opencv.global.opencv_imgcodecs.imread
+import org.bytedeco.opencv.opencv_core.Mat
 
 
 class Ai {
-   
-   private fun loadImage(fileName: String){
-     val src = imread(fileName)
-   }
+    private fun loadImage(fileName: String): Mat = imread(fileName)
+
 }
