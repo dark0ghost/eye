fb803922c94f260b04428106bc5b64913228d144
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-08-09 15:13:10 +0300

    fix: main.kt:20:5: Missing space after //

--- src/main/kotlin/org/dark0ghost/main.kt
+++ src/main/kotlin/org/dark0ghost/main.kt
@@ -1,6 +1,6 @@
 package org.dark0ghost
 
-import io.ktor.network.selector.*
+import io.ktor.network.selector.ActorSelectorManager
 import kotlinx.coroutines.Dispatchers
 import org.dark0ghost.enums.NetApi
 import org.dark0ghost.factory.ApiFactory
@@ -16,7 +16,7 @@ suspend fun main() {
     val address = InetSocketAddress(HOST_ADDRESS, PORT)
     val apiKtorBuilder: KtorApi.Builder = KtorApi.Builder().setSelector(selectorManager).setAddress(address)
     val apiFactory = (ApiFactory.getApi(NetApi.KtorApi) as KtorApiFactory).setBuilder(apiKtorBuilder)
-    //val ktorApi: KtorApi = apiFactory.createApi() as KtorApi
-    //ktorApi.getPhoto()
+    // val ktorApi: KtorApi = apiFactory.createApi() as KtorApi
+    // ktorApi.getPhoto()
+    // val eye: Eye = Eye.Builder().setApi(apiKtor).build()
 }
-// val eye: Eye = Eye.Builder().setApi(apiKtor).build()
